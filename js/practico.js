82deb3889c986e70d8b55b6e58a252b2b7301c45
carabajal-facundo <facarabajal@hotmail.com>
2023-03-20 23:01:02 -0300

    Se agrego la estructura basica

---
+++ js/practico.js
@@ -0,0 +1,8 @@
+let auto = {
+    color: "Naranja",
+    marca: "MCLaren",
+    modelo: "12c Spider",
+    encendido: false,
+    encenderAuto(){this.encendido=true},
+    apagarAuto(){this.encendido=false}
+}
