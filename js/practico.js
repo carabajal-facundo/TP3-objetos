1e5d8c3d5b56a60618b262255c26f8bebee19675
carabajal-facundo <facarabajal@hotmail.com>
2023-03-20 23:09:40 -0300

    Se agrego los puntos 1 y 2

--- js/practico.js
+++ js/practico.js
@@ -1,8 +1,37 @@
+// Punto 1
+
 let auto = {
-    color: "Naranja",
-    marca: "MCLaren",
-    modelo: "12c Spider",
+    color:'',
+    marca:'',
+    modelo:'',
     encendido: false,
+    constructor(color,marca,modelo,encendido){
+        this.color=color;
+        this.marca=marca;
+        this.modelo=modelo;
+        this.encendido=encendido;
+    },
     encenderAuto(){this.encendido=true},
     apagarAuto(){this.encendido=false}
 }
+
+// Punto 2
+
+let cuenta = {
+    titular: "Alex",
+    saldo: 0,
+    ingresar(dinero){this.saldo=dinero;},
+    extraer(dinero){this.saldo-=dinero;},
+    informar(){
+        document.write("Estado de la cuenta<br>");
+        document.write("Titular: "+this.titular+"<br>");
+        document.write("Saldo: "+this.saldo+"<br><br>");
+    }
+}
+
+cuenta.informar();
+cuenta.ingresar(200);
+cuenta.informar();
+cuenta.extraer(100);
+cuenta.informar();
+// git commit -m "Se agrego los puntos 1 y 2"
