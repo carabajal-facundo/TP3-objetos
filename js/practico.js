6dde8a7b9e1e6c8f41568fa59d86469bbec6526c
carabajal-facundo <facarabajal@hotmail.com>
2023-03-20 23:45:35 -0300

    Se agrego los puntos 3 y 4

--- js/practico.js
+++ js/practico.js
@@ -1,5 +1,5 @@
 // Punto 1
-
+document.write("<h1>Punto1</h1>");
 let auto = {
     color:'',
     marca:'',
@@ -16,7 +16,7 @@ let auto = {
 }
 
 // Punto 2
-
+document.write("<h1>Punto2</h1>");
 let cuenta = {
     titular: "Alex",
     saldo: 0,
@@ -34,4 +34,58 @@ cuenta.ingresar(200);
 cuenta.informar();
 cuenta.extraer(100);
 cuenta.informar();
-// git commit -m "Se agrego los puntos 1 y 2"
+document.write("<br><hr>")
+
+// Punto 3
+document.write("<h1>Punto3</h1>");
+class Rectangulo{
+    alto;
+    ancho;
+    constructor(alto, ancho){
+        this.alto=alto;
+        this.ancho=ancho;
+    }
+    modAlto(alto){this.alto=alto;}
+    modAncho(ancho){this.ancho=ancho;}
+    mostrarRectangulo(){
+        document.write("El ancho es: "+this.ancho+"<br>");
+        document.write("El alto es: "+this.alto+"<br>");
+    }
+    calcularPerimetro(){return 2*(this.alto+this.ancho)}
+    calcularArea(){return this.ancho*this.alto}
+}
+
+let r1 = new Rectangulo(10,4);
+r1.mostrarRectangulo();
+document.write("perimetro de r1: "+r1.calcularPerimetro()+"<br>");
+document.write("El area de r1: "+ r1.calcularArea()+"<br>")
+document.write("<br><hr>")
+
+// Punto 4
+document.write("<h1>Punto4</h1>");
+class Producto{
+    codigo;
+    nombre;
+    precio;
+    constructor(nombre,precio,codigo){
+        this.nombre=nombre;
+        this.precio=precio;
+        this.codigo=codigo;
+    }
+    impimirDatos(){
+        document.write("Producto: " + this.nombre + "<br>");
+        document.write("Precio: " + this.precio + "<br>");
+        document.write("Codigo: " + this.codigo + "<br><br>");
+    }
+}
+
+let productos = [
+    prod1 = new Producto("prod1",100,001),
+    prod2 = new Producto("prod2",200,002),
+    prod3 = new Producto("prod3",300,003)
+];
+
+for(let i=0; i<3; i++) productos[i].impimirDatos();
+document.write("<br><hr>")
+
+// Punto 5
